16689755e82579fa26c4e073a4678636d8abc6a2
tominaga <>
2018-01-31 16:53:04 +0900

    modify server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,35 @@
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
 const server = http.createServer(function(request, response) {
-  response.end('Hello world!');
+
+  const urlPath = url.parse(request.url).pathname;
+  let filePath = `./client/${urlPath}`;
+
+  fs.stat(filePath, function(err, fileInfo) {
+    if (!err && fileInfo.isDirectory()) {
+      filePath += '/index.html';
+    }
+
+    fs.exists(filePath, function(doesExist) {
+      if (!doesExist) {
+        response.statusCode = 404;
+        response.end(`Resource not found: "${urlPath}"`);
+      }
+
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          response.statusCode = 500;
+          response.end(`Server error: "${err}"`);
+        } else {
+          response.end(data.toString('UTF-8'));
+        }
+      });
+
+    });
+  });
+
 });
 
 server.listen(3000, function() {
