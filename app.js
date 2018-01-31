baa305188df719a15db5e0981c6d1c897d8ef6af
tominaga <>
2018-01-31 13:38:55 +0900

    modify app.js

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const add = require('./adder.js');
 
 class ViewManager {
-  
+
 	connectEventHandlers() {
-    // wire up event handler for form submit
+    document.getElementById('form-numbers')
+      .addEventListener(
+        'submit',
+        this.onSubmit);
   }
 
   onSubmit(event) {
-    // block form from acrually submitting
-    // (which would refresh the page)
+    event.preventDefault();
 
-    // grab th number values as strings
+    let num1 = document.getElementById('input-num1').value;
+    let num2 = document.getElementById('input-num2').value;
 
-    // cast th strings to ints
+    num1 = parseInt(num1, 10);
+    num2 = parseInt(num2, 10);
 
-    // add the numbers
+    const sum = add(num1, num2);
 
-    // output
+    alert(sum);
   }
 }
 
